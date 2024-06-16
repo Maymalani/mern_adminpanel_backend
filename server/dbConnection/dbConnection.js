e44f6f5a657f6942f12fef8b0ff1144c0607dc21
Mayank Malani <mayankmalani0@gmail.com>
2024-06-16 21:44:49 +0530

    file deleted

--- server/dbConnection/dbConnection.js
+++
@@ -1,4 +0,0 @@
-var mongoose = require('mongoose');
-
-module.exports = mongoose.connect(`${process.env.MONGO_URI}`)
-.then(() => console.log("dbConnect Successfully")).catch((e) => console.log(e))
--- server/middleware/errorMiddleware.js
+++
@@ -1,9 +0,0 @@
-var errorMiddleware = (err,req,res,next) => {
-    var status = err.status || 400;
-    var message = err.message || "BackEnd Error";
-    var extraDetails = err.extraDetails || "BackEnd Error";
-
-    return res.status(status).json({message,extraDetails});
-}
-
-module.exports = errorMiddleware;
