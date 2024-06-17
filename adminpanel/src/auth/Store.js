6acb31536c765b5d9b56ca8ecc051ffb9925407b
Mayank Malani <mayankmalani0@gmail.com>
2024-06-17 09:33:16 +0530

    change api

--- adminpanel/src/auth/Store.js
+++ adminpanel/src/auth/Store.js
@@ -12,7 +12,7 @@ export const AuthProvider = ({ children }) => {
     const [allUser, setAllUser] = useState([]);
     const [courseWoContent, setCourseWoContent] = useState([]);
     const [allCourseContent, setAllCourseContent] = useState([]);
-    const api = "https://mern-adminpanel1.vercel.app"
+    const api = "https://mern-adminpanel.vercel.app"
 
     const storeTokenInLs = (token) => {
         setToken(token);
